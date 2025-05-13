96bbdfb94947035becdbba127f170939a4ff976c
adarsh singh <adarsh.22bce7232@vitapstudent.ac.in>
2025-05-14 00:39:58 +0530

    parcel setting node modules

--- REACT DEVELOPMENT/class 02 Ignite our App/app.js
+++ REACT DEVELOPMENT/class 02 Ignite our App/app.js
@@ -1,4 +1,11 @@
-    const root = document.getElementById('root2');
-    const heading = document.createElement('h1');
-    heading.innerHTML='hello from js';
-    root.appendChild(heading);
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+    
+    // const root = document.getElementById('root2');
+    // const heading = document.createElement('h1');
+    // heading.innerHTML='hello from js';
+    // root.appendChild(heading);
+
+    const element = React.createElement('h1', null, 'hello from react');
+    const roots = ReactDOM.createRoot(document.getElementById('root'));
+    roots.render(element);
